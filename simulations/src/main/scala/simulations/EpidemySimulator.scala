219fc388cf5b37cc07ec38388c3a4ff5f894d722
Cosmin Cojocar <cosmin.cojocar@swisscom.com>
2014-09-09 16:29:29 +0200

    Implement the epidemy simulator

--- simulations/src/main/scala/simulations/EpidemySimulator.scala
+++ simulations/src/main/scala/simulations/EpidemySimulator.scala
@@ -2,6 +2,8 @@ package simulations
 
 import math.random
 
+case class Room(val row: Int, val col: Int)
+
 class EpidemySimulator extends Simulator {
 
   def randomBelow(i: Int) = (random * i).toInt
@@ -11,25 +13,120 @@ class EpidemySimulator extends Simulator {
     val roomRows: Int = 8
     val roomColumns: Int = 8
 
-    // to complete: additional parameters of simulation
+    val prevalenceRate = 0.01
+    val transRate = 0.4
+    val dieRate = 0.25
+
+    val incubationTime = 6
+    val dieTime = 14
+    val immuneTime = 16
+    val healTime = 18
+    val moveInTime = 5
   }
 
   import SimConfig._
 
-  val persons: List[Person] = List() // to complete: construct list of persons
+  val totalInfected = population * prevalenceRate
+  val persons: List[Person] = List.tabulate(population - 1) { id =>
+    val p = new Person(id + 1)
+    if (id + 1 <= totalInfected) p.infect
+    p.move
+    p
+  }
 
-  class Person (val id: Int) {
+  class Person(val id: Int) {
     var infected = false
     var sick = false
     var immune = false
     var dead = false
 
-    // demonstrates random number generation
     var row: Int = randomBelow(roomRows)
     var col: Int = randomBelow(roomColumns)
 
-    //
-    // to complete with simulation logic
-    //
+    private var actions: List[Action] = List()
+
+    def reset() {
+      infected = false
+      sick = false
+      immune = false
+      dead = false
+    }
+
+    def addAction(a: Action): Unit = {
+      actions = a :: actions
+      a()
+    }
+
+    def infect {
+      def infectAction(): Unit = {
+        infected = true
+        afterDelay(incubationTime) {
+          sick = true
+        }
+        afterDelay(dieTime) {
+          if (random.toDouble <= dieRate) dead = true
+        }
+        afterDelay(immuneTime) {
+          if (!dead) {
+            immune = true; sick = false
+          }
+        }
+        afterDelay(healTime) {
+          if (!dead) reset
+        }
+      }
+      addAction(infectAction)
+    }
+
+    def move {
+      def moveAction(): Unit = {
+        val moveInDay = randomBelow(moveInTime) + 1
+        afterDelay(moveInDay) {
+          if (!dead) {
+            val randomNeighbor = uninfectedRoom(neighborRooms(Room(row, col)))
+            randomNeighbor match {
+              case Some(r) => moveTo(r)
+              case None =>
+            }
+            addAction(moveAction)
+          }
+        }
+      }
+      addAction(moveAction)
+    }
+
+    def contaminated = sick | dead
+    def presentInRoom(room: Room) = room.row == row && room.col == col
+    def moveTo(room: Room) = {
+      row = room.row
+      col = room.col
+      if (!infected && random.toDouble <= transRate) infect
+    }
   }
+
+  def contaminatedRoom(room: Room): Boolean = {
+    persons.exists(p => p.presentInRoom(room) && p.contaminated)
+  }
+
+  def incubationRoom(room: Room): Boolean = {
+    persons.exists(p => p.presentInRoom(room) && p.infected)
+  }
+
+  def neighborRooms(room: Room): List[Room] = {
+    val left = (room.col - 1 + roomRows) % roomRows
+    val righ = (room.col + 1) % roomRows
+    val top = (room.row + 1 + roomColumns) % roomColumns
+    val bootom = (room.row + 1) % roomRows
+    List(left, righ).map(col => new Room(room.row, col)) ::: List(top, bootom).map(row => new Room(row, room.col))
+  }
+
+  def uninfectedRoom(rooms: List[Room]): Option[Room] = rooms match {
+    case Nil => None
+    case rs => {
+      val idx = (random * rs.length).toInt
+      if (!contaminatedRoom(rs(idx))) Some(rs(idx))
+      else uninfectedRoom(rooms diff List(rs(idx)))
+    }
+  }
+
 }
